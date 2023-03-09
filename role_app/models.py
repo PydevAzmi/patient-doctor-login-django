d5e1329509d1e149198542657278f2467a67e9a6
pydevazmi <pydevazmi@gmail.com>
2023-03-09 17:08:24 +0200

    Working on models

--- role_app/models.py
+++ role_app/models.py
@@ -5,3 +5,17 @@ from django.utils.translation import gettext as _
 class User(AbstractUser):
     is_patient = models.BooleanField(_("Is Patient"), default= False)
     is_doctor = models.BooleanField(_("Is Doctor"), default= False)
+
+class Paient(models.Model):
+    paient = models.OneToOneField("User",related_name ="Patient_User",
+                                   verbose_name=_("patient"), on_delete=models.CASCADE)
+    ph_num = models.CharField(_("Phone Number"), max_length=15)
+    survay = ''
+    report = ''
+    request = ''
+
+class Doctor (models.Model):
+    Doctor = models.OneToOneField("User", verbose_name=_("Doctor"),
+                                   related_name="Doctor_User", on_delete=models.CASCADE)
+    certificate = ''
+    specialist = ''
