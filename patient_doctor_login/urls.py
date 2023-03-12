f2fc646ad01c53512b1b498317b5a5932120f868
pydevazmi <pydevazmi@gmail.com>
2023-03-12 03:53:07 +0200

    urls editing

--- patient_doctor_login/urls.py
+++ patient_doctor_login/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path ,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls , name= "admin"),
+    path('admin/', admin.site.urls),
     path('', include('role_app.urls', namespace= 'role_app'))
 ]
